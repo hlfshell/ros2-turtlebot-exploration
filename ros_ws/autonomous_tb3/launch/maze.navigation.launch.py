533eccad3807913232938f04f888079a8ad7bd59
Keith Chester <kchester@gmail.com>
2023-08-18 19:18:20 -0700

    feature: started next section, WIP

--- ros_ws/autonomous_tb3/launch/maze.navigation.launch.py
+++ ros_ws/autonomous_tb3/launch/maze.navigation.launch.py
@@ -9,10 +9,12 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+    config_dir = os.path.join(get_package_share_directory("autonomous_tb3"), "config")
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     maze_path = os.path.join(get_package_share_directory('autonomous_tb3'), 'worlds', 'simple_maze', 'model.sdf')
+    rviz_config = os.path.join(config_dir, "tb3_nav.rviz")
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-9.32')
@@ -55,6 +57,26 @@ def generate_launch_description():
         arguments=[maze_path, "simple_maze", "0.0", "0.0"],
     )
 
+    mapping = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'mapping.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    rviz = Node(
+        package="rviz2",
+        output="screen",
+        executable="rviz2",
+        name="rviz2_node",
+        arguments=[
+            "-d", rviz_config
+        ],
+    )
+
     ld = LaunchDescription()
 
     # Add the commands to the launch description
@@ -63,5 +85,7 @@ def generate_launch_description():
     ld.add_action(robot_state_publisher_cmd)
     ld.add_action(spawn_turtlebot_cmd)
     ld.add_action(maze_spawner)
+    ld.add_action(mapping)
+    ld.add_action(rviz)
 
     return ld
